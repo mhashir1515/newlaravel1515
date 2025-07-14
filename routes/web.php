ca561e2f3c5217f7b6940192a40bb6f5819d3b41
muhammadhashir39 <175919409+mhashir1515@users.noreply.github.com>
2025-07-14 17:17:28 +0500

    Add Breeze authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/dashboard', function () {
     return view('dashboard', ['users' => $users]);
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/debug-error', function () {
+    throw new \Exception('Test error to trigger Laravel error logging');
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
