8f9f0fa750f34dc98f338ec8e3582c6f98e134e3
muhammadhashir39 <175919409+mhashir1515@users.noreply.github.com>
2025-07-14 16:46:19 +0500

    Add Breeze authentication scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,20 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-
 Route::get('/dashboard', function () {
-    $users = [];
-
-    // Only fetch users if admin
-    if (Auth::user()->is_admin) {
-        $users = \App\Models\User::all();
-    }
-
-    return view('dashboard', ['users' => $users]);
-})->middleware(['auth'])->name('dashboard');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::middleware('auth')->group(function () {
-    Route::get('/password/edit', [PasswordController::class, 'edit'])
-        ->name('password.edit');
-    Route::put('/password/update', [PasswordController::class, 'update'])
-        ->name('password.update');
 });
 
 require __DIR__.'/auth.php';
